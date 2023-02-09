ed212629e5042ff7bcc856470d150d7d9a9ee23b
samuel <samuel@Berry.local>
2023-02-09 07:46:41 +0100

    Added .env BASE_URL variable

--- src/components/ImageMasonry.vue
+++ src/components/ImageMasonry.vue
@@ -14,7 +14,7 @@ const loadData = async $state => {
 
     try {
         const response = await fetch(
-            "https://dog.ceo/api/breeds/image/random/50?page=" + page
+            `${import.meta.env.VITE_BASE_URL}/breeds/image/random/50?page=${page}`
         );
         const json = await response.json();
         if (json.message.length) {
@@ -31,7 +31,7 @@ const loadData = async $state => {
 </script>
 
 <template>
-    <div class="w-full grid gap-5 grid--masonry grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+    <div class="w-full grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         <Image :items="imageData" />
     </div>
     <div class="flex w-full justify-center items-center">
